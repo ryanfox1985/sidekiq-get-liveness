be2082176e8b65b526d16c467dd9f72fcccfd11f
Guillermo Guerrero <guillermo@guerreroibarra.com>
2024-10-07 16:57:30 +0200

    Empty message.

--- lib/sidekiq/get_liveness.rb
+++ lib/sidekiq/get_liveness.rb
@@ -30,10 +30,11 @@ module Sidekiq
           liveness_check(client, hostname, pid)
         else
           client.puts "HTTP/1.1 404 Not Found"
-          client.puts "Content-Length: #{"Not found".bytesize + 1}"
+          message = "Not found"
+          client.puts "Content-Length: #{message.bytesize + 1}"
           client.puts "Content-Type: text/plain"
           client.puts
-          client.puts "Not found"
+          client.puts message
         end
       end
 
@@ -42,18 +43,16 @@ module Sidekiq
           p["hostname"] == hostname && p["pid"] == pid
         end
 
-        message = if process.present?
-                    client.puts "HTTP/1.1 200 OK"
-                    "Sidekiq Worker #{hostname}-#{pid} is alive."
-                  else
-                    client.puts "HTTP/1.1 503 Service Unavailable"
-                    "Sidekiq Worker #{hostname}-#{pid} is not alive."
-                  end
-
-        client.puts "Content-Length: #{message.bytesize + 1}"
-        client.puts "Content-Type: text/plain"
-        client.puts
-        client.puts message
+        if process.present?
+          client.puts "HTTP/1.1 200 OK"
+        else
+          client.puts "HTTP/1.1 503 Service Unavailable"
+          message = "Sidekiq Worker #{hostname}-#{pid} is not alive."
+          client.puts "Content-Length: #{message.bytesize + 1}"
+          client.puts "Content-Type: text/plain"
+          client.puts
+          client.puts message
+        end
       end
     end
   end
